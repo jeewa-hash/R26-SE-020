ce24fb6f0e9e72ce4f1717a0c3407836f8a8683b
TashmiOshera <osheratashmi@gmail.com>
2026-05-06 20:42:07 +0530

    resolve multi-class evaluation issue

---
+++ Seeker_Service/models/Post.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  image: String,
+  category: String,
+  tags: [String],
+  urgency: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Post", postSchema);
